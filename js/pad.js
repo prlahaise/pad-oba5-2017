e233a557c574acb161a9d90542c00f5336f9ea09
prlahaise <prlahaise@users.noreply.github.com>
2017-05-02 16:21:45 +0200

    PopupOverlay Test & jQuery.load

--- js/pad.js
+++ js/pad.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
+
+// Initialize overlay plugin
+$('#my_popup').popup();
+$('#my_popup').load("test.html");
+
+// Check user input for 'toverwoorden'
 $("#invoer").on("change keyup paste", function(){
   var toverwoord = $('#invoer').val();
 
   switch(toverwoord) {
   	case "test":
-  		$("#p1").appendTo("#mvtest");
+  		$('#p1').appendTo("#mvtest");
   		$('#testimage01').css({"filter":"grayscale(0%)"});
   		break;
   	case "test2":
-  		$("#p2").appendTo("#mvtest");
+  		$('#p2').appendTo("#mvtest");
   		$('#testimage02').css({"filter":"grayscale(0%)"});
   		break;
   }
